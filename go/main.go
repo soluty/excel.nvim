9e05410407abcd0ec053d5796b2b24507c9e01c8
闾明 <lvming@mail.soluty.cc>
2024-02-26 23:49:41 +0800

    测试返回值可以为一个struct.

--- go/main.go
+++ go/main.go
@@ -1,31 +1,38 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"strings"
 
 	"github.com/neovim/go-client/nvim"
 	"github.com/neovim/go-client/nvim/plugin"
-	"github.com/tealeg/xlsx/v3"
 )
 
-func openExcel(v *nvim.Nvim, args []string) (string, error) {
-	if len(args) == 0 {
-		return "", errors.New("insert excel file name")
-	}
-	filename := args[0]
-	wb, err := xlsx.OpenFile(filename)
-	if err != nil {
-		return "", fmt.Errorf("cant open excel: %w", err)
-	}
-	if len(wb.Sheets) == 1 {
-		// open direct新开一个buffer, 在新的buffer里面打开excel文件.
-		sheet := wb.Sheets[0]
-		return sheet.Name, nil
-	}
-	// wb.Sheets
-	return "Hello " + strings.Join(args, " "), nil
+type MyStruct struct {
+	Name  string
+	Value string
+}
+
+func openExcel(v *nvim.Nvim, args []string) (*MyStruct, error) {
+	fmt.Println("pfrom golang")
+	return &MyStruct{
+		Name:  "name",
+		Value: "value",
+	}, nil
+	// if len(args) == 0 {
+	// 	return "", errors.New("insert excel file name")
+	// }
+	// filename := args[0]
+	// wb, err := xlsx.OpenFile(filename)
+	// if err != nil {
+	// 	return "", fmt.Errorf("cant open excel: %w", err)
+	// }
+	// if len(wb.Sheets) == 1 {
+	// 	// open direct新开一个buffer, 在新的buffer里面打开excel文件.
+	// 	sheet := wb.Sheets[0]
+	// 	return sheet.Name, nil
+	// }
+	// // wb.Sheets
+	// return "Hello " + strings.Join(args, " "), nil
 }
 
 func main() {
--- plugin/excel/init.lua
+++ plugin/excel/init.lua
@@ -15,40 +15,46 @@ vim.cmd([[
       \ ])
 ]])
 
-function RenderTableTest()
-	local NuiTable = require("nui.table")
-	local Text = require("nui.text")
-	local tbl = NuiTable({
-		bufnr = 0,
-		columns = {
-			{
-				align = "center",
-				header = "Name",
-				columns = {
-					{ accessor_key = "firstName", header = "First" },
-					{
-						id = "lastName",
-						accessor_fn = function(row)
-							return row.lastName
-						end,
-						header = "Last",
-					},
-				},
-			},
-			{
-				align = "right",
-				accessor_key = "age",
-				cell = function(cell)
-					return Text(tostring(cell.get_value()), "DiagnosticInfo")
-				end,
-				header = "Age",
-			},
-		},
-		data = {
-			{ firstName = "John", lastName = "Doe", age = 42 },
-			{ firstName = "Jane", lastName = "Doe", age = 27 },
-		},
-	})
+local M = {}
 
-	tbl:render()
+function RenderTableTest()
+	local result = vim.api.nvim_exec("let ret = ExcelNvimOpenFile('ss') | echo ret", true)
+	print(result)
+	-- local NuiTable = require("nui.table")
+	-- local Text = require("nui.text")
+	-- local tbl = NuiTable({
+	--   bufnr = 0,
+	--   columns = {
+	--     {
+	--       align = "center",
+	--       header = "Name",
+	--       columns = {
+	--         { accessor_key = "firstName", header = "First" },
+	--         {
+	--           id = "lastName",
+	--           accessor_fn = function(row)
+	--             return row.lastName
+	--           end,
+	--           header = "Last",
+	--         },
+	--       },
+	--     },
+	--     {
+	--       align = "right",
+	--       accessor_key = "age",
+	--       cell = function(cell)
+	--         return Text(tostring(cell.get_value()), "DiagnosticInfo")
+	--       end,
+	--       header = "Age",
+	--     },
+	--   },
+	--   data = {
+	--     { firstName = "John", lastName = "Doe", age = 42 },
+	--     { firstName = "Jane", lastName = "Doe", age = 27 },
+	--   },
+	-- })
+	--
+	-- tbl:render()
 end
+
+return M
